19f012067993340f6e16a2825d03f59d7473cd97
Lucas Gonzalez <luc4s92.g@gmail.com>
2024-01-16 20:51:08 -0300

    Numbers animation on archivo page created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  i18n:{
+  integrations: [tailwind(), preact()],
+  i18n: {
     defaultLocale: 'es',
-    locales: ['es','ca'],
-    routing:{
+    locales: ['es', 'ca'],
+    routing: {
       prefixDefaultLocale: false
-    },
-   
+    }
   }
 });
